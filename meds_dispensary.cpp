3604485afd20948cb2b531a0c17f78964793d633
Ken Aaker <kenaaker@gmail.com>
2014-04-06 16:45:54 -0500

    First functional load with state machine.

--- meds_dispensary.cpp
+++ meds_dispensary.cpp
@@ -1,41 +1,43 @@
 #include "meds_dispensary.h"
 #include "ui_meds_dispensary.h"
+#include "dispensary_fsm_sm.h"
 #include <QFontDatabase>
 #include <QtCore/qdebug.h>
 
 Meds_dispensary::Meds_dispensary(QWidget *parent) :
     QWidget(parent),
-    dispense_fsm(*this),
-    ui(new Ui::Meds_dispensary)
-{
-    dispense_fsm.enterStartState();
+    ui(new Ui::Meds_dispensary),
+    dispense_fsm(*this)
+ {
+
     ui->setupUi(this);
     m_dca = new motor(e_motor_id::e_motor_a);
     m_dcb = new motor(e_motor_id::e_motor_b);
     sr1 = new gpio_sensor("P9_25", "P9_39");
     sr2 = new gpio_sensor("P9_27", "P9_40");
+    dispense_fsm.enterStartState();
 
 //    QFontDatabase fonts;
 //    qDebug() << "fonts" << fonts.families();
 
 }
 
-void Meds_dispensary::quiesce() {
-    ;
-};
-
-void Meds_dispensary::start_working() {
-//    arena_setup();
-//    sweeps_setup();
-//    arena_center();
-//    sweeps_leveling();
-};
-
-Meds_dispensary::~Meds_dispensary()
-{
+Meds_dispensary::~Meds_dispensary() {
     delete sr1;
     delete sr2;
     delete m_dcb;
     delete m_dca;
     delete ui;
+}
+
+void Meds_dispensary::set_motor_a_speed(int speed) {
+    m_dca->motor_run(speed);
+}
+
+void Meds_dispensary::set_motor_b_speed(int speed) {
+    m_dcb->motor_run(speed);
+}
+
+void Meds_dispensary::set_status_label(QString info) {
+    ui->status_label->setText(info);
 }
--- meds_dispensary.h
+++ meds_dispensary.h
@@ -4,7 +4,7 @@
 #include <QtWidgets>
 #include "motor.h"
 #include "gpio_sensor.h"
-#include <dispensary_fsm_sm.h>
+#include "dispensary_fsm_sm.h"
 
 namespace Ui {
 class Meds_dispensary;
@@ -17,7 +17,9 @@ class Meds_dispensary : public QWidget
 public:
     explicit Meds_dispensary(QWidget *parent = 0);
     ~Meds_dispensary();
-    dispensary_fsmContext dispense_fsm;
+    void set_motor_a_speed(int);
+    void set_motor_b_speed(int);
+    void set_status_label(QString);
 
 private:
     Ui::Meds_dispensary *ui;
@@ -25,11 +27,8 @@ private:
     motor *m_dcb;
     gpio_sensor *sr1;
     gpio_sensor *sr2;
+    dispensary_fsmContext dispense_fsm;
 
-    void quiesce();
-    void start_working();
-
-    QStateMachine pill_handler_state;
 };
 
 #endif // MEDS_DISPENSARY_H
